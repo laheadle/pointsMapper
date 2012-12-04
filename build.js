a88b010c7f2dcfc34c9d22f84841bfdd49cceafd
Lyn Headley <laheadle@gmail.com>
2012-12-03 20:10:33 -0800

    city browser pages based on a points browser js lib

---
+++ build.js
@@ -0,0 +1,13 @@
+var jade = require('jade'),
+fs = require('fs');
+
+var fnames = ['bay-area-cities', 'big-california-cities', 'rhine-cities'];
+
+for (var i = 0;i< fnames.length;i++) {
+    var fname = './'+fnames[i]+'.jade';
+    var str = fs.readFileSync(fname, 'utf8');
+    var txt = jade.compile(str, {filename: fname})();
+    fs.writeFile('./_site/'+fnames[i]+'.html', txt);
+}
+
+
